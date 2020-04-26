d21dda9692aae84eb2316cd9a614e6c7af2820fa
AlexanderZap <chander.zap@gmail.com>
2020-04-26 15:12:52 -0500

    database connection

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+const { database } = require('./keys');
+
+
+const pool = mysql.createPool(database);
+
+
+pool.getConnection((err, connection) => {
+    if(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.log('DATABASE CONNECTION WAS LOST');
+        }
+        if(err.code === 'ER_CON_COUNT_ERROR'){
+            console.log('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if(err.code === 'ECONNREFUSED'){
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+
+    if(connection){
+        connection.release();
+        console.log('DB is connected');
+        return;
+    }
+});
+
+//  Promisify converts callbacks to promises
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
--- src/routes/data.js
+++ src/routes/data.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express();
+const pooldb = require('../database');
 
 
 module.exports = router;
