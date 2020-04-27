bb2931692484ad19e9ca3bc7eb9d4bc2f055a82a
AlexanderZap <chander.zap@gmail.com>
2020-04-27 03:35:40 -0500

    Problems with inserts logic, i hope solve it soon

--- src/routes/data.js
+++ src/routes/data.js
@@ -7,9 +7,27 @@ router.get('/add', (req, res) => {
     res.render('proyectos/add')
 });
 
-router.post('/add', (req, res) => {
-    res.send(req.body);
-    console.log(req.body);
+router.post('/add', async (req, res) => {
+    const {nombre, superficie, fecha_inicio, fecha_fin} = req.body;
+    const {nombre_propietario, apellido_propietario, telefono} = req.body;
+
+    const newPropietario = {
+        nombre: nombre_propietario,
+        apellido: apellido_propietario,
+        telefono: telefono
+    };
+
+    pooldb.query('INSERT INTO propietarios set ?', [newPropietario], (err, rows) => {
+        const { insertId } = rows;
+        
+
+    });
+    
+    res.send('recibido');
+    console.log(insertId);
+
+
+    
 });
 
 module.exports = router;
