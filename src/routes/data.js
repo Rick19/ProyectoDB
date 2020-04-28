b56723688a64ac6c4667eeff93a7e9bef0afa82f
AlexanderZap <chander.zap@gmail.com>
2020-04-28 00:40:28 -0500

    add a new project complete (project, date, owner)

--- src/routes/data.js
+++ src/routes/data.js
@@ -8,8 +8,10 @@ router.get('/add', (req, res) => {
 });
 
 router.post('/add', async (req, res) => {
-    const {nombre, superficie, fecha_inicio, fecha_fin} = req.body;
-    const {nombre_propietario, apellido_propietario, telefono} = req.body;
+    const { nombre, superficie, fecha_inicio, fecha_fin } = req.body;
+    const { nombre_propietario, apellido_propietario, telefono } = req.body;
+    const { calle, numero, num_interno, colonia, ciudad, estado } = req.body;
+
 
     const newPropietario = {
         nombre: nombre_propietario,
@@ -17,17 +19,41 @@ router.post('/add', async (req, res) => {
         telefono: telefono
     };
 
-    pooldb.query('INSERT INTO propietarios set ?', [newPropietario], (err, rows) => {
-        const { insertId } = rows;
-        
+    const newDomicilio = {
+        calle: calle,
+        numero: numero,
+        num_interno: num_interno,
+        colonia: colonia,
+        ciudad: ciudad,
+        estado: estado
+    }
 
-    });
-    
+    //Primero ingresamos un propietario
+    await pooldb.query('INSERT INTO propietarios set ?', [newPropietario]);
+    //  Luego solicitamos el ID del propietario que acabamos de ingresar
+    const id_prop = await pooldb.query('SELECT id FROM propietarios WHERE nombre LIKE ? AND apellido LIKE ?', [nombre_propietario, apellido_propietario]);
+    //  Ahora insertamos la direccion
+    // console.log(id_prop[0].id);console.log(id_direct[0].id);
+    await pooldb.query('INSERT INTO direccion set ?', [newDomicilio]);
+    const id_direct = await pooldb.query('SELECT id FROM direccion WHERE `calle` = ? AND `numero` = ? AND `num_interno` = ? AND `colonia` = ? AND `ciudad` = ? AND `estado` = ?',
+        [calle, numero, num_interno, colonia, ciudad, estado]);
+
+    const newProject = {
+        nombre: nombre,
+        superficie: superficie,
+        fecha_inicio: fecha_inicio,
+        fecha_fin: fecha_fin,
+        id_propietario: id_prop[0].id,
+        id_direccion: id_direct[0].id
+    }
+
+    await pooldb.query('INSERT INTO proyectos set?', [newProject]);
+
     res.send('recibido');
-    console.log(insertId);
 
 
-    
+
+
 });
 
 module.exports = router;
